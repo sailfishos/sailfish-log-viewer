cca136280a899ce9f7246bbf5e980dc1846af6cf
Slava Monich <slava@monich.com>
2026-06-10 06:23:54 +0300

    [sailfish-log-viewer] Made tmp directory configurable via dconf
    There's no UI for it (yet) but at least it can be configured from the
    command line.

--- logger/src/LoggerSettings.cpp
+++ logger/src/LoggerSettings.cpp
@@ -1,33 +1,40 @@
 /*
- * Copyright (C) 2016-2017 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2026 Slava Monich <slava@monich.com>
+ * Copyright (C) 2015-2017 Jolla Ltd.
  *
- * You may use this file under the terms of BSD license as follows:
+ * You may use this file under the terms of the BSD license as follows:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of Jolla Ltd nor the names of its contributors may
- *      be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "LoggerSettings.h"
@@ -40,6 +47,7 @@
 #define DCONF_FONT_SIZE_ADJUSTMENT(app) DCONF_KEY(app,"fontSizeAdjustment")
 #define DCONF_AUTO_ENABLE_LOGGING(app)  DCONF_KEY(app,"autoEnableLogging")
 #define DCONF_AUTO_RESET_LOGGING(app)   DCONF_KEY(app,"autoResetLogging")
+#define DCONF_TMP_DIR(app)              DCONF_KEY(app,"tmpDir")
 
 //===========================================================================
 // LoggerSettings::Private
@@ -49,47 +57,43 @@ class LoggerSettings::Private : public QObject
 {
     Q_OBJECT
 public:
-    Private(QString aApp, QObject* aParent);
+    Private(const QString&, QObject*);
 
-    int logSizeLimit() const;
-    int fontSizeAdjustment() const;
     AutoEnable autoEnableLogging() const;
-    bool autoResetLogging() const;
 
 public:
     static const int DEFAULT_LOG_SIZE_LIMIT = 1000;
     static const int DEFAULT_FONT_SIZE_ADJUSTMENT = 0;
     static const int DEFAULT_AUTO_ENABLE_LOGGING = AutoEnableAll;
     static const bool DEFAULT_AUTO_RESET_LOGGING = true;
+    static const QString DEFAULT_TMP_DIR;
 
     MGConfItem* iLogSizeLimit;
     MGConfItem* iFontSizeAdjustment;
     MGConfItem* iAutoEnableLogging;
     MGConfItem* iAutoResetLogging;
+    MGConfItem* iTmpDir;
 };
 
-LoggerSettings::Private::Private(QString aApp, QObject* aParent) :
+const QString LoggerSettings::Private::DEFAULT_TMP_DIR("/tmp");
+
+LoggerSettings::Private::Private(
+    const QString& aApp,
+    QObject* aParent) :
     QObject(aParent),
     iLogSizeLimit(new MGConfItem(DCONF_LOG_SIZE_LIMIT(aApp), this)),
     iFontSizeAdjustment(new MGConfItem(DCONF_FONT_SIZE_ADJUSTMENT(aApp), this)),
     iAutoEnableLogging(new MGConfItem(DCONF_AUTO_ENABLE_LOGGING(aApp), this)),
-    iAutoResetLogging(new MGConfItem(DCONF_AUTO_RESET_LOGGING(aApp), this))
-{
-}
+    iAutoResetLogging(new MGConfItem(DCONF_AUTO_RESET_LOGGING(aApp), this)),
+    iTmpDir(new MGConfItem(DCONF_TMP_DIR(aApp), this))
+{}
 
-inline int LoggerSettings::Private::logSizeLimit() const
-{
-    return iLogSizeLimit->value(DEFAULT_LOG_SIZE_LIMIT).toInt();
-}
-
-inline int LoggerSettings::Private::fontSizeAdjustment() const
-{
-    return iFontSizeAdjustment->value(DEFAULT_FONT_SIZE_ADJUSTMENT).toInt();
-}
-
-inline LoggerSettings::AutoEnable LoggerSettings::Private::autoEnableLogging() const
+inline
+LoggerSettings::AutoEnable
+LoggerSettings::Private::autoEnableLogging() const
 {
     int value = iAutoEnableLogging->value(DEFAULT_AUTO_ENABLE_LOGGING).toInt();
+
     switch(value) {
     case AutoEnableNone:
     case AutoEnableAll:
@@ -98,16 +102,13 @@ inline LoggerSettings::AutoEnable LoggerSettings::Private::autoEnableLogging() c
     return (AutoEnable)DEFAULT_AUTO_ENABLE_LOGGING;
 }
 
-inline bool LoggerSettings::Private::autoResetLogging() const
-{
-    return iAutoResetLogging->value(DEFAULT_AUTO_RESET_LOGGING).toBool();
-}
-
 //===========================================================================
 // LoggerSettings::Private
 //===========================================================================
 
-LoggerSettings::LoggerSettings(QString aApp, QObject* aParent) :
+LoggerSettings::LoggerSettings(
+    QString aApp,
+    QObject* aParent) :
     QObject(aParent),
     iPrivate(new Private(aApp, this))
 {
@@ -121,29 +122,44 @@ LoggerSettings::LoggerSettings(QString aApp, QObject* aParent) :
         this, SIGNAL(autoResetLoggingChanged()));
 }
 
-int LoggerSettings::defaultLogSizeLimit()
+int
+LoggerSettings::defaultLogSizeLimit()
 {
     return Private::DEFAULT_LOG_SIZE_LIMIT;
 }
 
-int LoggerSettings::logSizeLimit() const
+int
+LoggerSettings::logSizeLimit() const
 {
-    return iPrivate->logSizeLimit();
+    return iPrivate->iLogSizeLimit->
+        value(Private::DEFAULT_LOG_SIZE_LIMIT).toInt();
 }
 
-int LoggerSettings::fontSizeAdjustment() const
+int
+LoggerSettings::fontSizeAdjustment() const
 {
-    return iPrivate->fontSizeAdjustment();
+    return iPrivate->iFontSizeAdjustment->
+        value(Private::DEFAULT_FONT_SIZE_ADJUSTMENT).toInt();
 }
 
-LoggerSettings::AutoEnable LoggerSettings::autoEnableLogging() const
+LoggerSettings::AutoEnable
+LoggerSettings::autoEnableLogging() const
 {
     return iPrivate->autoEnableLogging();
 }
 
-bool LoggerSettings::autoResetLogging() const
+bool
+LoggerSettings::autoResetLogging() const
 {
-    return iPrivate->autoResetLogging();
+    return iPrivate->iAutoResetLogging->
+        value(Private::DEFAULT_AUTO_RESET_LOGGING).toBool();
+}
+
+QString
+LoggerSettings::tmpDir() const
+{
+    return iPrivate->iTmpDir->
+        value(Private::DEFAULT_TMP_DIR).toString();
 }
 
 #include "LoggerSettings.moc"
--- logger/src/LoggerSettings.h
+++ logger/src/LoggerSettings.h
@@ -1,47 +1,55 @@
 /*
- * Copyright (C) 2016 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2026 Slava Monich <slava@monich.com>
+ * Copyright (C) 2015-2016 Jolla Ltd.
  *
- * You may use this file under the terms of BSD license as follows:
+ * You may use this file under the terms of the BSD license as follows:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of Jolla Ltd nor the names of its contributors may
- *      be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #ifndef LOGGER_SETTINGS_H
 #define LOGGER_SETTINGS_H
 
-#include <QtQml>
+#include <QtCore/QObject>
 
-class LoggerSettings : public QObject
+class LoggerSettings :
+    public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int logSizeLimit READ logSizeLimit NOTIFY logSizeLimitChanged)
     Q_PROPERTY(int fontSizeAdjustment READ fontSizeAdjustment NOTIFY fontSizeAdjustmentChanged)
     Q_PROPERTY(bool autoEnableLogging READ autoEnableLogging NOTIFY autoEnableLoggingChanged)
-    Q_PROPERTY(bool autoResetLogging READ autoResetLogging NOTIFY autoResetLoggingChanged)
+    Q_PROPERTY(QString tmpDir READ tmpDir NOTIFY tmpDirChanged)
 
 public:
     enum AutoEnable {
@@ -49,7 +57,7 @@ public:
         AutoEnableAll
     };
 
-    explicit LoggerSettings(QString aAppName, QObject* aParent = NULL);
+    explicit LoggerSettings(QString, QObject* aParent = Q_NULLPTR);
 
     static int defaultLogSizeLimit();
 
@@ -57,18 +65,18 @@ public:
     int fontSizeAdjustment() const;
     AutoEnable autoEnableLogging() const;
     bool autoResetLogging() const;
+    QString tmpDir() const;
 
 Q_SIGNALS:
     void logSizeLimitChanged();
     void fontSizeAdjustmentChanged();
     void autoEnableLoggingChanged();
     void autoResetLoggingChanged();
+    void tmpDirChanged();
 
 private:
     class Private;
     Private* iPrivate;
 };
-
-QML_DECLARE_TYPE(LoggerSettings)
 
 #endif // LOGGER_SETTINGS_H
